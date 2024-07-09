ed052e8142a898da57e0d5e2f1423a89ce29aa26
Saran v v <154489707+saranvv@users.noreply.github.com>
2024-07-09 14:51:21 +0530

    Time: 0 ms (100%), Space: 41.6 MB (52.12%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,29 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-      int n=digits.length;
-       long sum=0;
-        for(int i=0;i<digits.length;i++){
-            sum=sum*10+digits[i];
+        int n = digits.length;
+
+        for(int i = n-1; i >= 0; i--){
+            digits[i]++;
+
+            if(digits[i] < 10){
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
         }
-        long tot=sum+1;
-       String str=String.valueOf(tot);
-        int[] arr=new int[str.length()];
-        int i =0;
-        while(tot!=0){
-            long mod=tot%10;
-            arr[i]=(int)mod;
-            i++;
-            tot/=10;
-        }
-        return disp(arr);
-    }
-    public int[] disp(int[] arr){
-        int[] pls=new int[arr.length];
-        int k=0;
-        for(int i=arr.length-1;i>=0;i--){
-            pls[k]=arr[i];
-            k++;
-        }
-        return pls;
+
+        int[] newDigits = new int[n+1];
+        newDigits[0] = 1;
+        return newDigits;
     }
 }
