e978d979bfed55efc841138a45bb718dc9c34936
Saran v v <154489707+saranvv@users.noreply.github.com>
2024-07-12 16:58:04 +0530

    Time: 164 ms (53.54%), Space: 46.4 MB (45.45%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
@@ -5,31 +5,42 @@ class Solution {
         int sum = 0;
         
         if (y > x) {
-            int temp = x;
-            x = y;
-            y = temp;
-            s = new StringBuilder(s).reverse().toString();
-        }
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (!st1.isEmpty() && st1.peek() == 'a' && s.charAt(i) == 'b') {
-                st1.pop();
-                sum += x;
-            } else {
-                st1.push(s.charAt(i));
+            for (int i = 0; i < s.length(); i++) {
+                if (!st1.empty() && st1.peek() == 'b' && s.charAt(i) == 'a') {
+                    st1.pop();
+                    sum += y;
+                } else {
+                    st1.push(s.charAt(i));
+                }
+            }
+            while (!st1.isEmpty()) {
+                char c = st1.pop();
+                if (!st2.isEmpty() && st2.peek() == 'b' && c == 'a') {
+                    sum += x;
+                    st2.pop();
+                } else {
+                    st2.push(c);
+                }
+            }
+        } else {
+            for (int i = 0; i < s.length(); i++) {
+                if (!st1.empty() && st1.peek() == 'a' && s.charAt(i) == 'b') {
+                    st1.pop();
+                    sum += x;
+                } else {
+                    st1.push(s.charAt(i));
+                }
+            }
+            while (!st1.isEmpty()) {
+                char c = st1.pop();
+                if (!st2.isEmpty() && st2.peek() == 'a' && c == 'b') {
+                    sum += y;
+                    st2.pop();
+                } else {
+                    st2.push(c);
+                }
             }
         }
-        
-        while (!st1.isEmpty()) {
-            char c = st1.pop();
-            if (!st2.isEmpty() && st2.peek() == 'b' && c == 'a') {
-                st2.pop();
-                sum += y;
-            } else {
-                st2.push(c);
-            }
-        }
-        
         return sum;
     }
 }
