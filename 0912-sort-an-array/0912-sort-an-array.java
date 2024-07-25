bfb560dfc343016175d6d75faab4bde358f08405
Saran v v <154489707+saranvv@users.noreply.github.com>
2024-07-25 17:40:44 +0530

    Time: 18 ms (86.59%), Space: 55.9 MB (57.59%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        /*
+        for(int i=0;i<nums.length-1;i++){
+         for(int j=0;j<nums.length-1-i;j++){
+            if(nums[j]>nums[j+1]){
+                int t=nums[j];
+                nums[j]=nums[j+1];
+                nums[j+1]=t;
+            }
+         }
+        }
+        */
+        Arrays.sort(nums);
+            return nums;
+         }
+        
+}
