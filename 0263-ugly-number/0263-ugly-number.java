0187be00d9356b6695724318c70798fe497f435a
Saran v v <154489707+saranvv@users.noreply.github.com>
2024-07-26 15:24:35 +0530

    Time: 1 ms (50.47%), Space: 40.6 MB (52.2%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,13 +1,22 @@
 class Solution {
-  public boolean isUgly(int n)
-   {
-    while (n >= 5 && n % 5 == 0) 
-    n /= 5;
-    while (n >= 3 && n % 3 == 0)
-     n /= 3;
-    while (n >= 2 && n % 2 == 0)
-     n /= 2;
-
-    return n == 1;
-  }
+    public boolean isUgly(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n!=1){
+            if(n%2!=0 && n%3!=0 && n%5!=0 && n%6!=0 && n%10!=0 && n%15!=0){
+                return false;
+            }
+            if(n%2==0){
+                n=n/2;
+            }
+            else if(n%3==0){
+                n=n/3;
+            }
+            else if(n%5==0){
+                n=n/5;
+            }
+        }
+        return true;
+    }
 }
