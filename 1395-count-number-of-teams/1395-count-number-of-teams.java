8f5ce6a1fe469548b39f1ac2dc8c8abc72e3d4e4
Saran v v <154489707+saranvv@users.noreply.github.com>
2024-07-29 14:57:30 +0530

    Time: 2156 ms (6.48%), Space: 42.3 MB (52.18%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.java
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -1,8 +1,8 @@
 class Solution {
     public int numTeams(int[] r) {
         int count=0;
-        for(int i=0;i<r.length-2;i++){
-            for(int j=i+1;j<r.length-1;j++){
+        for(int i=0;i<r.length;i++){
+            for(int j=i+1;j<r.length;j++){
                        for(int k=j+1;k<r.length;k++){
                         if((r[i]<r[j])&&(r[j]<r[k])) count++;
                         if((r[i]>r[j])&&(r[j]>r[k])) count++;
