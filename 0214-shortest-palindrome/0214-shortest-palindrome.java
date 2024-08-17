17b5a6a192a66257ba41a9085b41b4691f6e09b6
Saran v v <154489707+saranvv@users.noreply.github.com>
2024-08-17 11:36:43 +0530

    Time: 44 ms (30.6%), Space: 45.9 MB (5.22%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,1 +1,25 @@
-s
+class Solution {
+    public String shortestPalindrome(String s) {
+         Stack<Character> st=new Stack<>();
+        for (char c:s.toCharArray()) {
+            st.push(c);
+        }
+        
+        StringBuilder sb= new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+        
+        String rev =sb.toString();
+          int l=0;
+        for (int i = 0;i<s.length();i++) {
+            if (s.startsWith(rev.substring(i))) {
+                l=i;
+                break;
+            }
+        }
+        
+        String ans=rev.substring(0,l);
+        return ans+s;
+    }
+}
