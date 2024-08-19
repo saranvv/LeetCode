4ae0e294adcba020980a767adf683e1eac9f237f
Saran v v <154489707+saranvv@users.noreply.github.com>
2024-08-19 14:58:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int x=-1;
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j<t.length();j++){
+            if(s.charAt(i)==t.charAt(j) && j>x){
+                count++;
+                x=j;
+            }
+            }
+        }
+            if(count==s.length()) return true;
+            return false;
+
+    }
+}
