f43fb68ceb9d9cbd2817d5680218d21c8622dc85
Saran v v <154489707+saranvv@users.noreply.github.com>
2024-08-20 16:32:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] f, int n) {
+        int l=f.length;
+        if(l<2){
+            if(f[0]==0 && n==1) return true;
+            return false;
+        }
+
+        if(f[0]==0 && f[1]==0) {
+            f[0]=1;
+            n--;
+        }
+        if(f[l-1]==0 && f[l-2]==0){
+            f[l-1]=1;
+            n--;
+        }
+        for(int i=1;i<l-1;i++){
+            if(f[i]==0 ){
+                     if(f[i-1]==0 && f[i+1]==0){
+                        f[i]=1;
+                        n--;
+                     }
+            }
+        }
+        if(n<=0) return true;
+        return false;
+    }
+}
