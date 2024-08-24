d333cfc4a14a236de9819044ba7b9a5a217b47ab
Saran v v <154489707+saranvv@users.noreply.github.com>
2024-08-24 15:03:54 +0530

    Time: 11 ms (94.66%), Space: 46.3 MB (84.7%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.java
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.java
@@ -1,34 +1,17 @@
 class Solution {
     public int minSteps(String s, String t) {
-     int count=0;
-     char[] c1=s.toCharArray();
-     char[] c2=t.toCharArray();
-   int k=0;
-         for(int i=0;i<c1.length;i++){
-              boolean bool=false;
-            for(int j=0;j<c2.length;j++){
-                if(c1[i]==c2[j]) {
-                    bool=true;
-                    k=j;
-            }
-            }
-            if(bool) c2[k]='#';
-            else count++;
-            
-         }
-          char[] a1=t.toCharArray();
-     char[] a2=s.toCharArray();
-        for(int i=0;i<a1.length;i++){
-              boolean bool=false;
-            for(int j=0;j<a2.length;j++){
-                if(a1[i]==a2[j]) {
-                    bool=true;
-                    k=j;
-            }
-            }
-            if(bool) a2[k]='#';
-            else count++;
-         }
+        int count=0;
+
+     int[] arr=new int[26];
+     for(int i=0;i<s.length();i++){
+        arr[s.charAt(i)-'a']++;
+     }
+      for(int i=0;i<t.length();i++){
+        arr[t.charAt(i)-'a']--;
+     }
+     for(int i=0;i<arr.length;i++){
+       count+=Math.abs(arr[i]);
+     }
      return count;
 
     }
